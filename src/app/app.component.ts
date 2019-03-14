a9927348b902e4d62b86f5f7f5e12524792dc8c1
Carolina Da Silva <mdasilvh@everis.com>
2019-03-14 18:34:50 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var H: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'map-food';
+
+  public query: string;
+
+  public constructor () {
+    this.query = 'starbucks';
+  }
+
+  public ngOnInit() { }
+
 }
---
+++ src/app/here-map/here-map.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, ViewChild, ElementRef, Input } from '@angular/core';
+
+declare var H: any;
+
+@Component({
+  selector: 'app-here-map',
+  templateUrl: './here-map.component.html',
+  styleUrls: ['./here-map.component.css']
+})
+export class HereMapComponent implements OnInit {
+
+  private ui: any;
+  private search: any;
+
+  @ViewChild('map')
+  public mapElement: ElementRef;
+
+  @Input()
+  public appId: any;
+
+  @Input()
+  public appCode: any;
+
+  @Input()
+  public lat: any;
+
+  @Input()
+  public lng: any;
+
+  @Input()
+  public width: any;
+
+  @Input()
+  public height: any;
+
+  private platform: any;
+  private map: any;
+
+  public constructor () { }
+
+  public ngOnInit() {
+    this.platform = new H.service.Platform({
+      'app_id': this.appId,
+      'app_code': this.appCode
+    });
+  }
+
+  public ngAfterViewInit() {
+    const defaultLayers = this.platform.createDefaultLayers();
+    this.map = new H.Map(
+      this.mapElement.nativeElement,
+      defaultLayers.normal.map,
+      {
+        zoom: 10,
+        center: { lat: this.lat, lng: this.lng }
+      }
+    );
+  }
+
+
+  public places(query: string) {
+    this.map.removeObjects(this.map.getObjects());
+    this.search.request({ 'q': query, 'at': this.lat + ',' + this.lng }, {}, data => {
+      for (let i = 0; i < data.results.items.length; i++) {
+        this.dropMarker({ 'lat': data.results.items[i].position[0], 'lng': data.results.items[i].position[1] }, data.results.items[i]);
+      }
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  private dropMarker(coordinates: any, data: any) {
+    const marker = new H.map.Marker(coordinates);
+    marker.setData('<p>' + data.title + '<br>' + data.vicinity + '</p>');
+    marker.addEventListener('tap', event => {
+      const bubble = new H.ui.InfoBubble(event.target.getPosition(), {
+        content: event.target.getData()
+      });
+      this.ui.addBubble(bubble);
+    }, false);
+    this.map.addObject(marker);
+  }
+
+
+}
